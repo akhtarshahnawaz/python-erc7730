4f4947a267aef67cea377fd7939b18197967a46b
Julien Nicoulaud <julien.nicoulaud@ledger.fr>
2024-09-18 18:42:01 +0200

    improve format

--- src/erc7730/main.py
+++ src/erc7730/main.py
@@ -31,8 +31,8 @@ def lint(
 
     for output in outputs:
         p=output.file.name if output.file is not None else "unknown file"
-        print(f"[red]{p}: {output.level.name}: {output.title}\n"
-              f"    {output.message}[/red]")
+        print(f"[red]{p}: {output.level.name}: {output.title}[/red]\n"
+              f"    {output.message}")
 
     if not outputs:
         print("[green]no issues found ✅[/green]")
