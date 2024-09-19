237d36442796911904d8442c3979275a884da823
Julien Nicoulaud <julien.nicoulaud@ledger.fr>
2024-09-19 11:56:57 +0200

    add GHA annotations output mode

--- src/erc7730/main.py
+++ src/erc7730/main.py
@@ -24,14 +24,24 @@ app = typer.Typer(
 def lint(
     path: Annotated[Path, typer.Argument(help="The file path")],
     demo: Annotated[bool, typer.Option(help="Enable demo mode")] = False,
+    gha: Annotated[bool, typer.Option(help="Enable Github annotations output")] = False,
 ) -> None:
     from erc7730.linter.lint import lint_all
+    from erc7730.linter import Linter
 
     outputs = lint_all(path, demo)
 
     for output in outputs:
         p = output.file.name if output.file is not None else "unknown file"
         print(f"[red]{p}: {output.level.name}: {output.title}[/red]\n" f"    {output.message}")
+        if gha:
+            match output.level:
+                case Linter.Output.Level.INFO:
+                    print(f"::notice file={output.file}::{output.title}: {output.message}")
+                case Linter.Output.Level.WARNING:
+                    print(f"::warning file={output.file}::{output.title}: {output.message}")
+                case Linter.Output.Level.ERROR:
+                    print(f"::error file={output.file}::{output.title}: {output.message}")
 
     if not outputs:
         print("[green]no issues found ✅[/green]")
