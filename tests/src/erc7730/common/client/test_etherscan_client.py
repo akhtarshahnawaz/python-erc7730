fa68ef7ca16246a65425a09454053049ec17bb14
Julien Nicoulaud <julien.nicoulaud@ledger.fr>
2024-09-20 09:20:42 +0200

    skip test that requires API key for now

--- tests/src/erc7730/common/client/test_etherscan_client.py
+++ tests/src/erc7730/common/client/test_etherscan_client.py
@@ -1,5 +1,8 @@
 from erc7730.common.client.etherscan import get_contract_abis
 
+import pytest
 
+
+@pytest.mark.skip(reason="Secret management not implemented")
 def test_get_contract_abis() -> None:
     assert len(get_contract_abis("0x06012c8cf97bead5deae237070f9587f8e7a266d")) > 0
